7727e4472c140850bb8cef8f23bc4b80704854ad
Liz Borchardt <zardra@gmail.com>
2016-03-04 15:58:44 -0800

    Initial Commit

---
+++ AlphaTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlphaTouch
+//
+//  Created by Liz Borchardt on 3/4/16.
+//  Copyright © 2016 Elizabeth Borchardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
